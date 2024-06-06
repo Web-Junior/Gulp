5f195b07884793cc9afd27156bdb91e29925c9bd
Nikita Khveshchenko <khveshchenkon@outlook.com>
2024-06-06 16:33:52 +0300

    feat: add modified the image task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -3,11 +3,12 @@ import { path } from "../config/path.js";
 import browserSync from "browser-sync";
 import gulpIf from "gulp-if";
 import { isBuild } from "../../gulpfile.js";
-import imageCompress from "compress-images";
 import imagemin from "gulp-imagemin";
+import newer from "gulp-newer";
 
 export const images = () => {
   return src(path.src.images, { encoding: false })
+    .pipe(newer(path.build.images))
     .pipe(
       gulpIf(
         isBuild,
